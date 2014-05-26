3d198f56163d33ea34c5c40664f30b3dd9ba0571
Neil Boyd <NeilBoyd@gmail.com>
2014-05-26 22:23:28 +0200

    Initial version Based on: http://docs.xamarin.com/guides/android/advanced_topics/java_integration_overview/binding_a_java_library_(.jar)/ https://github.com/xamarin/monodroid-samples/tree/master/OsmDroidBindingExample

---
+++ OsmdroidAndroidSample/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.OS;
+using Osmdroid.Api;
+using Osmdroid.TileProvider.TileSource;
+using Osmdroid.Util;
+using Osmdroid.Views;
+
+namespace OsmdroidAndroidSample
+{
+    [Activity(Label = "OsmdroidAndroidSample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private IMapController _mapController;
+        private MapView _mapView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            _mapView = FindViewById<MapView>(Resource.Id.mapview);
+            _mapView.SetTileSource(TileSourceFactory.DefaultTileSource);
+            _mapView.SetBuiltInZoomControls(true);
+
+            _mapController = _mapView.Controller;
+            _mapController.SetZoom(25);
+
+            var centreOfMap = new GeoPoint(51496994, -134733);
+            _mapController.SetCenter(centreOfMap);
+        }
+    }
+}
+
