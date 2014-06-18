dcb71a8322dd8fa882bd8534dbf4307205961b4b
Jeremy Kolb <jkolb@ara.com>
2014-06-18 12:55:23 -0400

    Add bindings for osmdroid-thirdparty.

---
+++ OsmdroidThirdpartyAndroidBinding/Additions/BingMapTileSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Runtime;
+
+namespace Osmdroid.TileProvider.TileSource.Bing
+{    
+    public partial class BingMapTileSource
+    {
+        public void SetStyle(string style)
+        {
+            this.Style = style;
+        }
+
+        Java.Lang.Object IStyledTileSource.Style
+        {
+            get { return new Java.Lang.String(Style); }
+            set { Style = (string)value.JavaCast<Java.Lang.String>(); }
+        }
+    }
+}
---
+++ OsmdroidThirdpartyAndroidBinding/Additions/MapView.cs
@@ -0,0 +1,13 @@
+﻿using Android.Graphics;
+using Osmdroid.Api;
+
+namespace Osmdroid.Google.Wrapper
+{
+    public partial class MapView
+    {
+        public void SetBackgroundColor(Color color)
+        {
+            ((IMapView)this).SetBackgroundColor(color);
+        }
+    }
+}
