886d6d9cdb39c33c266f2739a68ef34132e29287
Jeremy Kolb <jkolb@ara.com>
2014-06-19 09:41:46 -0400

    Rename OsmdroidThirdpartyAndroidBinging to OsmdroidThirdPartyBinding

--- OsmdroidThirdPartyBinding/Properties/AssemblyInfo.cs
+++ OsmdroidThirdPartyBinding/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OsmdroidThirdpartyAndroidBinding")]
+[assembly: AssemblyTitle("OsmdroidThirdPartyBinding")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("OsmdroidThirdpartyAndroidBinding")]
+[assembly: AssemblyProduct("OsmdroidThirdPartyBinding")]
 [assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
